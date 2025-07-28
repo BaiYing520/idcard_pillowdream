862f33b81fb0f8188ad47c43b5eec1d419b4d1c0
BaiYing520 <baiyingmc@outlook.com>
2025-07-29 06:10:30 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
-setup(name='idcard_pillowdream',
-      version='1',
-      description='idcard_pillowdream',
-      author='PillowDreamManagementGroup',
-      author_email='op@pillowdream.cn',
+setup(name='idcard_pillowdream',  
+      version='1',  # 版本为1
+      description='idcard_pillowdream',  
+      author='PillowDreamManagementGroup',  
+      # author_email='airobot@airobot.link'
       license='GPL-3.0',
-      packages=['idcard_pillowdream'],
-      # data_files=['idcard_pillowdream/asserts/empty.png', 'idcard_pillowdream/asserts/fzhei.ttf', 'idcard_pillowdream/asserts/hei.ttf', 'idcard_pillowdream/asserts/ico.icns', 'idcard_pillowdream/asserts/ocrb10bt.ttf'],
+      packages=['idcard_pillowdream'],  
       include_package_data=True,
       install_requires=['numpy==2.2.6', 'pillow==11.3.0', 'opencv-python==4.12.0.88']
       )
